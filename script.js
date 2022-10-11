356b8fed3b2318d78fa939bff605baccb64b8a0b
Kevin Schumilo <kevuschka@outlook.com>
2022-10-11 15:25:23 +0200

    pretty final version

--- script.js
+++ script.js
@@ -59,51 +59,84 @@ function checkForWin() {
     fullField();
 }
 
-
+// ########## HORIZONTAL LINES ##########
 function renderHorizontalLines() {
+    renderLine1();
+    renderLine2();
+    renderLine3();
+}
+
+function renderLine1() {
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
-        document.getElementById('line-1').style.transform = 'scaleX(1)';
-        gameOver = true;
-    }
-    else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
-        document.getElementById('line-2').style.transform = 'scaleX(1)';
-        gameOver = true;
-    }
-    else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
-        document.getElementById('line-3').style.transform = 'scaleX(1)';
+        document.getElementById('line-1').classList.add('show-horizontal-line');
         gameOver = true;
     }
 }
 
 
+function renderLine2() {
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        // document.getElementById('line-2').style.transform = 'scaleX(1)';
+        document.getElementById('line-2').classList.add('show-horizontal-line');
+        gameOver = true;
+    }
+}
+
+
+function renderLine3() {
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        document.getElementById('line-3').classList.add('show-horizontal-line');
+        gameOver = true;
+    }
+}
+
+// ########## VERTICAL LINES ##########
 function renderVerticalLines() {
+    renderLine4();
+    renderLine5();
+    renderLine6();
+}
+
+
+function renderLine4() {
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
-        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
-        gameOver = true;
-    }
-    else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
-        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
-        gameOver = true;
-    }
-    else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
-        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
+        // document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
+        document.getElementById('line-4').classList.add('show-vertical-line');
         gameOver = true;
     }
 }
 
 
+function renderLine5() {
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        document.getElementById('line-5').classList.add('show-vertical-line');
+        gameOver = true;
+    }
+}
+
+
+function renderLine6() {
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        document.getElementById('line-6').classList.add('show-vertical-line');
+        gameOver = true;
+    }
+}
+
+// ########## DIAGONAL LINES ##########
 function renderDiagonalLines() {
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
-        document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
+        // document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
+        document.getElementById('line-7').classList.add('show-diagonal-line-right');
         gameOver = true;
     }
     else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
-        document.getElementById('line-8').style.transform = 'rotate(45deg) scaleX(1.2)';
+        // document.getElementById('line-8').style.transform = 'rotate(45deg) scaleX(1.2)';
+        document.getElementById('line-8').classList.add('show-diagonal-line-left');
         gameOver = true;
     }
 }
 
-
+// ########## NO ONE WINS - FIELD FULL ##########
 function fullField() {
     if(fields[0]&&fields[1]&&fields[2]&&fields[3]&&fields[4]&&fields[5]&&fields[6]&&fields[7]&&fields[8]) {
         gameOver = true;
@@ -125,7 +158,7 @@ function showGameOver(x) {
 
 function showButton() {
     setTimeout(function() {
-        document.getElementById('restart-btn').style.transform = 'translateY(26rem) translateX(-33rem)';
+        document.getElementById('restart-btn').classList.add('show-restart-btn');
     },2000)
 }
 
@@ -135,7 +168,7 @@ function restart() {
     gameOver = false;
     document.getElementById('game-over').style.transform = 'scaleX(0.0)';
     document.getElementById('game-over').style.opacity = '0';
-    document.getElementById('restart-btn').style.transform = 'translateY(-550px) translateX(-33rem)';
+    document.getElementById('restart-btn').classList.remove('show-restart-btn');
     cleanFields();
 }
 
@@ -152,11 +185,13 @@ function cleanFields() {
 
 function cleanLines() {
     for (let i = 1; i < 4; i++) {
-        document.getElementById(`line-${i}`).style.transform = 'scaleX(0.0)';
+        // document.getElementById(`line-${i}`).style.transform = 'scaleX(0.0)';
+        document.getElementById(`line-${i}`).classList.remove('show-horizontal-line');
     }
     for (let i = 4; i < 7; i++) {
-        document.getElementById(`line-${i}`).style.transform = 'rotate(90deg) scaleX(0.0)';
+        document.getElementById(`line-${i}`).classList.remove('show-vertical-line');
     }
-    document.getElementById(`line-7`).style.transform = 'rotate(-45deg) scaleX(0.0)';
-    document.getElementById(`line-8`).style.transform = 'rotate(45deg) scaleX(0.0)';
+    // document.getElementById(`line-7`).style.transform = 'rotate(-45deg) scaleX(0.0)';
+    document.getElementById(`line-7`).classList.remove('show-diagonal-line-right');
+    document.getElementById(`line-8`).classList.remove('show-diagonal-line-left');
 }
